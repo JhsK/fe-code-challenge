741bf63943593204d2148e54083acf3d9f0a6bce
임성규 <sk.lim@moyoplan.com>
2025-09-20 14:42:41 +0900

    feat: vitest 관련 패키지 설치 및 테스트 환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+/// <reference types="vitest" />
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
+  },
 });
