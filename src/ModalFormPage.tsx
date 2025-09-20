e369fca8a83e1ee62a812307a8e1793272beb610
임성규 <sk.lim@moyoplan.com>
2025-09-20 14:17:51 +0900

    feat: 버튼 스타일 구현

--- src/ModalFormPage.tsx
+++ src/ModalFormPage.tsx
@@ -1,6 +1,11 @@
 const ModalFormPage = () => {
-  /* 여기에 구현해 주세요 */
-  return <div>ModalFormPage</div>;
+  return (
+    <section className="w-screen h-screen flex justify-center items-center">
+      <button className="rounded-lg bg-blue-500 text-white px-4 py-3 hover:opacity-90 cursor-pointer">
+        🚀 신청 폼 작성하기
+      </button>
+    </section>
+  );
 };
 
 export default ModalFormPage;
